2ed059660d9fa94faea8c19339543aa7abd26943
Hernan Carrizo <Hernan Carrizo>
2017-03-16 20:19:08 +0000

    add timer callback to hide the window after 8 seconds

--- xcopilot/xplane/window.py
+++ xcopilot/xplane/window.py
@@ -1,25 +1,29 @@
 from XPLMDisplay import *
 from XPLMGraphics import *
+from XPLMProcessing import *
 
 HEIGHT = 80
 MARGIN = 20
+TIMER = 8
 
 class Window:
 
     def __init__(self, plugin):
         self.plugin = plugin
-        self.isVisible = 0
         self.message = None
         self.width = self._getScreenWidth() - (2 * MARGIN)
 
         self.drawWindowCB = self.drawWindowCallback
-        self.windowId = XPLMCreateWindow(self.plugin, MARGIN, 100, MARGIN + self.width, 100 - HEIGHT, self.isVisible, self.drawWindowCB, 0, 0, 0)
+        self.windowId = XPLMCreateWindow(self.plugin, MARGIN, 100, MARGIN + self.width, 100 - HEIGHT, 0, self.drawWindowCB, 0, 0, 0)
+
+        self.timerLoopCB = self.timerLoopCallback
+        XPLMRegisterFlightLoopCallback(self.plugin, self.timerLoopCB, 0, 0)
 
     def show(self, message):
-        self.isVisible = 1
         self.message = message
-        XPLMSetWindowIsVisible(self.windowId, self.isVisible)
+        XPLMSetWindowIsVisible(self.windowId, 1)
         XPLMBringWindowToFront(self.windowId)
+        XPLMSetFlightLoopCallbackInterval(self.plugin, self.timerLoopCB, TIMER, 1, 0)
 
     def drawWindowCallback(self, inWindowID, inRefcon):
         lLeft = []; lTop = []; lRight = []; lBottom = []
@@ -31,8 +35,13 @@ class Window:
             gResult = XPLMDrawString(colour, left + 5, top - 20, self.message, 0, xplmFont_Basic)
         pass
 
+    def timerLoopCallback(self, elapsedMe, elapsedSim, counter, refcon):
+        XPLMSetWindowIsVisible(self.windowId, 0)
+        return 0
+
     def close(self):
         XPLMDestroyWindow(self.plugin, self.windowId)
+        XPLMUnregisterFlightLoopCallback(self.plugin, self.timerLoopCB, 0)
 
     def _getScreenWidth(self):
         width = []
