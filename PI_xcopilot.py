545d3b692e58610f74fd51f6c8a62a7b6cd0135e
Hernan Carrizo <Hernan Carrizo>
2017-03-22 23:44:46 +0000

    #2 refactor X-Plane plugin to use XCopilot main class

--- PI_xcopilot.py
+++ PI_xcopilot.py
@@ -2,10 +2,19 @@ from XPLMDataAccess import *
 from XPLMUtilities  import *
 from XPLMPlugin     import *
 from XPLMDefs       import *
-from xcopilot import Recognizer, parseCommand
+from xcopilot import XCopilot
 from xcopilot.xplane import Window
 import speech_recognition as sr
 import threading
+
+XPLM_MSG_PLANE_LOADED = 102
+
+SetDataRef = {
+    'float': XPLMSetDataf,
+    'int': XPLMSetDatai,
+    'boolean': XPLMSetDatai,
+    'double': XPLMSetDatad
+}
 
 class PythonInterface:
     def XPluginStart(self):
@@ -13,6 +22,7 @@ class PythonInterface:
         self.Sig = "Owentar.X-Copilot"
         self.Desc = "A voice commanded copilot"
         self.window = Window(self)
+        self.xcopilot = XCopilot()
 
         self.command = XPLMCreateCommand("owentar/xcopilot/record_voice_command", "Record voice command.")
         self.recordVoiceCB = self.recordVoiceCallback
@@ -32,7 +42,8 @@ class PythonInterface:
         pass
 
     def XPluginReceiveMessage(self, inFromWho, inMessage, inParam):
-        pass
+        if inMessage == XPLM_MSG_PLANE_LOADED:
+            # TODO: load config for the airplane
 
     def recordVoiceCallback(self, inCommand, inPhase, inRefcon):
         if inPhase == 0:
@@ -42,26 +53,9 @@ class PythonInterface:
 
     def recordVoice(self):
         self.window.show('Recording...')
-        r = Recognizer()
-        with sr.Microphone() as source:
-            audio = r.listen(source)
-
-        try:
-            # for testing purposes, we're just using the default API key
-            # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-            # instead of `r.recognize_google(audio)`
-            strCommand = r.recognize_sphinx2(audio)
-            self.window.show('Recorded: ' + strCommand)
-            command = parseCommand(strCommand)
-            if command is None:
-                self.window.show('Recorded: ' + strCommand + ' --> Command unrecognized: ' + strCommand)
-            else:
-                self.window.show('Command recognized: ' + command.name + ':' + str(command.value))
-                for name, value in command.dataRefs.iteritems():
-                    dataRef = XPLMFindDataRef(name)
-                    XPLMSetDataf(dataRef, value)
-        except sr.UnknownValueError:
-            self.window.show('Sphinx Speech Recognition could not understand audio')
-        except sr.RequestError as e:
-            self.window.show('Could not request results from Google Speech Recognition service; {0}'.format(e))
-        pass
+        result = self.xcopilot.recordCommand()
+        command = result[0]
+        dataRefs = result[1]
+        for dataRef in dataRefs:
+            dataRefID = XPLMFindDataRef(dataRef)
+            SetDataRef[command.type](dataRefID, command.value)
